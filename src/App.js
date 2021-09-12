f80675aa51dc9c468a455cda4a939753ffab309a
Konstantin <banan03112000@gmail.com>
2021-09-12 20:13:39 +0600

    [*] added PropTypes added fake backend

--- src/App.js
+++ src/App.js
@@ -2,16 +2,29 @@ import {Header} from "./components";
 import {Cart, Home} from "./pages";
 import React from "react";
 import {Route} from "react-router-dom";
+import axios from "axios";
 
 function App() {
+   const [pizzaItems, setPizzaItems] = React.useState([]);
+
+   React.useEffect(() => {
+      async function getData() {
+         const pizzas = await axios.get('http://localhost:3001/pizzas');
+
+         setPizzaItems(pizzas.data)
+      }
+
+      getData();
+   }, [])
+
    return (
-      <div className="wrapper">
-         <Header/>
-         <div className="content">
-            <Route path = "/" component = {Home} exact/>
-            <Route path = "/cart" component = {Cart} exact/>
+         <div className="wrapper">
+            <Header/>
+            <div className="content">
+               <Route path="/" render={() => <Home items={pizzaItems}/>} exact/>
+               <Route path="/cart" component={Cart} exact/>
+            </div>
          </div>
-      </div>
    );
 }
 
