e8400c14d8a230c5fd397ca44e9894e62801795c
Konstantin <banan03112000@gmail.com>
2021-09-14 16:49:31 +0600

    [*] App switched to redux hooks

--- src/App.js
+++ src/App.js
@@ -1,58 +1,34 @@
 import React from "react";
 import axios from "axios";
-import {Header} from "./components";
-import {Cart, Home} from "./pages";
 import {Route} from "react-router-dom";
-import {connect} from 'react-redux'
+import {Header} from "./components";
 import {setPizzas} from "./redux/actions/pizzas";
+import {Cart, Home} from "./pages";
+import {useSelector, useDispatch} from "react-redux";
 
-// function App() {
-//
-//    React.useEffect(() => {
-//       async function getData() {
-//          const {data} = await axios.get('http://localhost:3001/pizzas');
-//          setPizzaItems(data)
-//       }
-//
-//       getData();
-//    }, [])
-//
-//    return (
+function App() {
+   const dispatch = useDispatch();
+   const {items} = useSelector(({pizzas}) => {
+      return {
+         items: pizzas.items
+      }
+   })
 
-//    );
-// }
+   React.useEffect(() => {
+      axios.get('http://localhost:3000/DB.json').then(({data}) => {
+         dispatch(setPizzas(data.pizzas));
+      })
+   }, []);
 
-
-class App extends React.Component {
-   componentDidMount() {
-      axios.get('http://localhost:3001/pizzas').then(({data}) => {
-         this.props.setPizzas(data.pizzas)
-      });
-   }
-
-   render() {
-      return (
-         <div className="wrapper">
-            <Header/>
-            <div className="content">
-               <Route path="/" render={() => <Home items={this.props.items}/>} exact/>
-               <Route path="/cart" component={Cart} exact/>
-            </div>
+   return (
+      <div className="wrapper">
+         <Header/>
+         <div className="content">
+            <Route path="/" render={() => <Home items={items}/>} exact/>
+            <Route path="/cart" component={Cart} exact/>
          </div>
-      )
-   }
+      </div>
+   );
 }
 
-export default connect(
-   (state) => {
-      return {
-         items: state.pizzas.items,
-         filters: state.filters,
-      };
-   },
-   (dispatch) => {
-      return {
-         setPizzas: (items) => dispatch(setPizzas(items)),
-      };
-   },
-)(App);
+export default App;
